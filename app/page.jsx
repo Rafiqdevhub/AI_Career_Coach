67f32c11a2f3d3857ae0e436f262311259a9913a
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-29 15:20:08 +0500

    setup the shadcn for styling

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Wel come</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Home;
