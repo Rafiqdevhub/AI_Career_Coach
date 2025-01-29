428c24496eefa37c1518e5be06a9a517eb6ae5da
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-29 19:00:25 +0500

    Clerk is added for Authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,23 +15,34 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
         <body className={`${inter.className}`}>
-          <main className="min-h-screen">{children}</main>
-          <Toaster richColors />
-          <footer className="bg-muted/50 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Ideas Into Realities Through Coding</p>
-            </div>
-          </footer>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Turning Ideas Into Realities.</p>
+              </div>
+            </footer>
+          </ThemeProvider>
         </body>
-      </ThemeProvider>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
