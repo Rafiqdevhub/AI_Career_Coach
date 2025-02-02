03b9529d36ce9ffbd44e7a040ea678a123d10e71
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-02 21:09:04 +0500

    add cover letter generation and management features, including preview, list, and CRUD actions

--- app/(main)/ai-cover-letter/[id]/page.jsx
+++ app/(main)/ai-cover-letter/[id]/page.jsx
@@ -1,8 +1,25 @@
-const AiCoverLetter = async ({ params }) => {
-  const id = await params.id;
+import { getCoverLetter } from "@/actions/coverLetter";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import CoverLetterPreview from "../_components/CoverLetterPreview";
+
+const AiCoverLetter = async () => {
+  const coverLetter = await getCoverLetter(id);
   return (
     <div>
-      <h1>AI Cover Letter {id}</h1>
+      <div className="flex flex-col space-y-2">
+        <Link href="/ai-cover-letter">
+          <Button variant="link" className="gap-2 pl-0">
+            <ArrowLeft className="h-4 w-4" />
+            Back to Cover Letters
+          </Button>
+        </Link>
+        <h1 className="text-6xl font-bold gradient-title mb-6">
+          {coverLetter?.jobTitle} at {coverLetter?.companyName}
+        </h1>
+      </div>
+      <CoverLetterPreview content={coverLetter?.content} />
     </div>
   );
 };
--- app/(main)/ai-cover-letter/page.jsx
+++ app/(main)/ai-cover-letter/page.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import CoverLetterList from "./_components/CoverLetterList";
+import { getCoverLetters } from "@/actions/coverLetter";
 
-const CoverLetterPage = () => {
-  return <div>Ai Cover Letter</div>;
+const CoverLetterPage = async () => {
+  const coverLetters = await getCoverLetters();
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row gap-2 items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">My Cover Letters</h1>
+        <Link href="/ai-cover-letter/new">
+          <Button>
+            <Plus className="h-4 w-4 mr-2" />
+            Create New
+          </Button>
+        </Link>
+      </div>
+      <CoverLetterList coverLetters={coverLetters} />
+    </div>
+  );
 };
 
 export default CoverLetterPage;
